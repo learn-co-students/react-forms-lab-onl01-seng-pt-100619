df9aceb0a824771dbb8314efa2410398db64661f
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-07-03 14:34:53 +0200

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,51 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleUsernameInputChange = (event) => {
+  
+    this.setState({
+      username: event.target.value
+    })
+    // console.log(this.state.username)
+  }
+
+  handlePasswordInputChange = (event) => {
+  
+    this.setState({
+      password: event.target.value
+    })
+    // console.log(this.state.username)
+  }
+
+  handleSubmit = (event) => { // always send event listener to same class and then callback parent class function from here
+    event.preventDefault()
+    if(this.state.username !== '' && this.state.password !== ''){
+      this.props.handleLogin(this.state)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input value={this.state.username} onChange={this.handleUsernameInputChange} id="username" name="username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input value={this.state.password} onChange={this.handlePasswordInputChange} id="password" name="password" type="password" />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,44 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      chars: this.props.maxChars,
+      message: ''
+    };
+  }
+
+  updateChars = (event) => {
+    // let counter = this.state.chars
+
+    if(event.target.value.length < this.state.message.length){
+      // console.log('+1')
+      this.setState(previousState => {
+        return {chars: previousState.chars + 1} //return keyword makes the update state work ?creates a closure?
+      })
+    } else if(event.target.value.length > this.state.message.length){
+      // console.log('-1')
+      this.setState(previousState => {
+        return {chars: previousState.chars - 1}
+      })
+    }
+
+    this.setState({
+      message: event.target.value
+    })
+    // console.log(this.state.message)
+    // console.log(this.state.chars)
+
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={event => this.updateChars(event)} value={this.state.message}type="text" name="message" id="message" />
+        <p>{this.state.chars} characters remaining</p>
       </div>
     );
   }
