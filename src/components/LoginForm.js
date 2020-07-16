9cf41c591264e1ac7a6ddca6da9d5e7604fa4d38
Wendy Harris <wendyv510@hotmail.com>
2020-07-16 16:51:33 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,57 @@
 import React from "react";
+import PropTypes from 'prop-types' 
 
 class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+        username: '',
+        password: ''
+    };
+  }
+
+  handleInputChange = event => {
+    const {name, value} = event.target 
+    this.setState({ 
+      [name]: value,
+    })
+  }
+
+  handleSubmit = event => { 
+    event.preventDefault();
+     let formData ={ 
+       username: this.state.username,
+       password: this.state.password 
+     }
+      if (formData.username != "" && formData.password !=""){
+        this.props.handleLogin(formData)
+      }  
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit= {event => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+             id="username" 
+             name="username"
+             type="text"
+             value={this.state.username}
+             onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+             id="password"
+             name="password"
+             type="password" 
+             value={this.state.username}
+             onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
@@ -28,6 +60,10 @@ class LoginForm extends React.Component {
       </form>
     );
   }
+}
+
+LoginForm.propTypes = { 
+  onSubmit: PropTypes.func,
 }
 
 export default LoginForm;
